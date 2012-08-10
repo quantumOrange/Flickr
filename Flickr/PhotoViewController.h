eea9743c33c5b8c59faa4ec6acf95279ddab8fe5
David Crooks <crodave@gmail.com>
2012-08-10 19:14:06 +0200

    added photView and photoViewController to display selected photos.

---
+++ Flickr/PhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.h
+//  Flickr
+//
+//  Created by David Crooks on 10/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController
+
+@end
---
+++ Flickr/PhotoViewController.m
@@ -0,0 +1,57 @@
+//
+//  PhotoViewController.m
+//  Flickr
+//
+//  Created by David Crooks on 10/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PhotoViewController.h"
+#import "SubtileTableViewController.h"
+#import "FlickrFetcher.h"
+@interface PhotoViewController () <SubtileTableViewControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+@implementation PhotoViewController 
+@synthesize photoView;
+
+-(void) TVC:(SubtileTableViewController *) sender choseTableCell:(NSDictionary *)photo
+{
+    NSURL *photoURL = [FlickrFetcher urlForPhoto:photo format:FlickrPhotoFormatLarge];
+    NSData *imageData = [NSData dataWithContentsOfURL:photoURL];
+    UIImage *image =[UIImage imageWithData:imageData];
+    self.photoView.image = image;
+   
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [self setPhotoView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
